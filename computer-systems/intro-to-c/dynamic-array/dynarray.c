b9abaf3d2ce6be7a70593c6eafb7b07820dc9d46
acjr1910 <acjr1910@gmail.com>
2025-11-18 21:20:27 +0000

    wip: c dynamic array

---
+++ computer-systems/intro-to-c/dynamic-array/dynarray.c
@@ -0,0 +1,110 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#define STARTING_CAPACITY 8
+
+typedef struct DA
+{
+  int count;
+  int capacity;
+  void **items;
+} DA;
+
+DA *DA_new(void)
+{
+  DA *array = malloc(sizeof(DA));
+
+  array->count = 0;
+  array->capacity = STARTING_CAPACITY;
+  array->items = malloc(sizeof(void *) * STARTING_CAPACITY);
+
+  return array;
+}
+
+int DA_size(DA *da)
+{
+  return da->count;
+}
+
+void DA_push(DA *da, void *x)
+{
+  da->items[da->count++] = x;
+}
+
+void *DA_pop(DA *da)
+{
+  if (da->count == 0)
+  {
+
+    return NULL;
+  }
+  return da->items[--da->count];
+}
+
+void DA_set(DA *da, void *x, int i)
+{
+  da->items[i] = x;
+}
+
+void *DA_get(DA *da, int i)
+{
+  return da->items[i];
+}
+
+void DA_free(DA *da)
+{
+  free(da->items);
+  free(da);
+}
+
+int main()
+{
+  DA *da = DA_new();
+
+  assert(DA_size(da) == 0);
+
+  // basic push and pop test
+  int x = 5;
+  float y = 12.4;
+  DA_push(da, &x);
+  DA_push(da, &y);
+  assert(DA_size(da) == 2);
+
+  assert(DA_pop(da) == &y);
+  assert(DA_size(da) == 1);
+
+  assert(DA_pop(da) == &x);
+  assert(DA_size(da) == 0);
+  assert(DA_pop(da) == NULL);
+
+  // basic set/get test
+  DA_push(da, &x);
+  DA_set(da, &y, 0);
+  assert(DA_get(da, 0) == &y);
+  DA_pop(da);
+  assert(DA_size(da) == 0);
+
+  // expansion test
+  DA *da2 = DA_new(); // use another DA to show it doesn't get overriden
+  DA_push(da2, &x);
+  int i, n = 100 * STARTING_CAPACITY, arr[n];
+  for (i = 0; i < n; i++)
+  {
+    arr[i] = i;
+    DA_push(da, &arr[i]);
+  }
+  assert(DA_size(da) == n);
+  for (i = 0; i < n; i++)
+  {
+    assert(DA_get(da, i) == &arr[i]);
+  }
+  for (; n; n--)
+    DA_pop(da);
+  assert(DA_size(da) == 0);
+  assert(DA_pop(da2) == &x); // this will fail if da doesn't expand
+
+  DA_free(da);
+  DA_free(da2);
+  printf("OK\n");
+}
